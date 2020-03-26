b8e1250ebe1958d12d42de6d53670b5801d0979c
Gabriel Jony <gabjony@gmail.com>
2020-03-25 22:48:29 -0300

    7 Aula 2 Configurando as rota da ongs para o cadastrar e buscar ongs

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,43 +1,10 @@
-const express = require('express');
+const express = require("express");
+
+const OngController = require("./controllers/OngController");
 
 const routes = express.Router();
-/**
- * Rota / Recurso
- */
-/**
- * Métodos HTTP
- * 
- * GET: Buscar uma informação no back-end
- * POST: Criar uma informação no back-end
- * PUT: Alterar uma informação no back-end
- * DELETE: Deletar uma informação no back-end
- * 
- */
 
-/**
- * Tipos de parâmetros
- * 
- * Query Params: Parâmetros nomeados enviados na rota após "?" (Filtros, paginação)
- * Route Params: Parâmetros utilizados para identificar recursos
- * Request Body: Corpo da Requisição, utilizado para criar ou alterar recursos
- * 
- */
-
-/**
- * Banco de dados SQL
- * Usando o banco de dados SQLite
- */
-
-/**
- * Driver: SELECT * FROM users
- * Query Builder: table('users').select('*').where()
- * usando a query builder: http://knexjs.org/
- */
-routes.get('/users', (request, response) => {
-  return response.json({
-    evento: 'Semana OmniStack 11.0',
-    aluno: 'Gabriel Jony'
-  });
-})
+routes.get("/ongs", OngController.index);
+routes.post("/ongs", OngController.create);
 
 module.exports = routes;
