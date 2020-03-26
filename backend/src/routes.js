29e3eba22feed9990f5ed85a6dff4c1daee182de
Gabriel Jony <gabjony@gmail.com>
2020-03-25 23:07:30 -0300

    9 Aula 2 configuração  da rota session

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -3,6 +3,7 @@ const express = require("express");
 const OngController = require("./controllers/OngController");
 const IncidentsController = require("./controllers/IncidentsController");
 const ProfileController = require("./controllers/ProfileController");
+const SessionController = require("./controllers/SessionController");
 
 const routes = express.Router();
 
@@ -15,5 +16,6 @@ routes.delete("/incidents/:id", IncidentsController.delete);
 
 routes.get("/profile", ProfileController.index);
 
+routes.post("/sessions", SessionController.create);
 
 module.exports = routes;
