e9ab3195ee5527deba838c8c38817f944cab177f
Gabriel Jony <gabjony@gmail.com>
2020-03-29 09:26:14 -0300

    14 Aula 3 Conceito Estado e Imutabilidade

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { useState }from 'react';
 import Header from './Header';
 
 function App() {
+
+  let [counter, setCounter] = useState(0);
+  //Array [valor, funçãodeatualização]
+
+  function increment() {
+    setCounter(counter + 1);
+  }
+
   return (
     <>
-      <Header title="Semana OmniStack"/>
+      <Header title="Semana OmniStack" />
       <Header>Segundo Texto</Header>
+      <Header>Contador: {counter}</Header>
+      <button onClick={increment}>Incrementar</button>
     </>
   );
 }
